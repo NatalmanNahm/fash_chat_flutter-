0d26391a403a7e714bfa8091cac110f19ea8aa57
Natalman <natalnahm@gmail.com>
2020-05-01 11:47:57 -0500

    Modified the UI so that the user messages are seperated from the messages sent by other user.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -169,7 +169,7 @@ class MessageBubble extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: <Widget>[
           Text(
             sender,
@@ -179,8 +179,10 @@ class MessageBubble extends StatelessWidget {
             ),
           ),
           Material(
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0),
-            bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0)),
+            borderRadius: isMe ? BorderRadius.only(topLeft: Radius.circular(30.0),
+                bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0))
+            : BorderRadius.only(topRight: Radius.circular(30.0),
+                bottomLeft: Radius.circular(30.0), bottomRight: Radius.circular(30.0)),
             elevation: 5.0,
             color: isMe ? Colors.lightBlueAccent : Colors.white,
             child: Padding(
